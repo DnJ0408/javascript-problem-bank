afaa9459c09379eda9b6d1b6fb0a73af76ca08f1
YoungJin <oyj921205@gmail.com>
2025-01-16 12:17:29 +0900

    20250116

--- part1/06.배열/(lv.2)배열-메서드-find.js
+++ part1/06.배열/(lv.2)배열-메서드-find.js
@@ -20,10 +20,18 @@
  */
 
 // TODO: findUserByName 함수를 작성하세요.
-function findUserByName() {}
+function findUserByName(users, name) {
+    return users.find(function (user) {
+        return user.name === name;
+    }) || null;
+}
 
 // TODO: findIndexOf 함수를 작성하세요.
-function findIndexOf() {}
+function findIndexOf(arr, value) {
+    return arr.findIndex(function (element) {
+        return value === element;
+    })
+}
 
 // export를 수정하지 마세요.
 export { findUserByName, findIndexOf };
