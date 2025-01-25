ffa5ab12196ca2ac347c45d37a9ece1d9a40d46c
YoungJin <oyj921205@gmail.com>
2025-01-25 21:30:28 +0900

    20250125

--- part2/02.배열/(lv.3)데이터-변환-파이프라인.js
+++ part2/02.배열/(lv.3)데이터-변환-파이프라인.js
@@ -14,7 +14,13 @@
  * @returns {number[]} 변환된 배열
  */
 
-function transformData(arr, threshold) {}
-
+function transformData(arr, threshold) {
+    
+    return arr
+    .filter((num) => num % 2 === 0)
+    .map((eachNum) => eachNum = eachNum ** 2)
+    .filter((evenNum) => evenNum <= threshold)
+    .sort((a, b) => b - a);
+}
 // export 를 수정하지 마세요.
 export { transformData };
